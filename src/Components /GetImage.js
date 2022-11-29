0119beb55b5d24730d86503ab72fc9c608344436
Zacharia <ndunguzachary24@gmail.com>
2022-11-29 10:39:21 +0300

    fetch data functionality

--- src/Components /GetImage.js
+++ src/Components /GetImage.js
@@ -3,8 +3,12 @@ import React, { useState, useEffect} from 'react'
 const GetImage = () => {
     const [images, setImages] = useState([])
     useEffect(() => {
-    
-        
+        const fetchData = async () => {
+            const resp = await fetch()
+            const data = resp.json()
+            setImages(data)
+    }
+
 }, [])
     use
   return (
